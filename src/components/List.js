34cd02a81778999b7ddf82512ba17ca4526ffe07
Erickcamp <thecampkid@gmail.com>
2020-05-14 09:55:26 -0600

    done

---
+++ src/components/List.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react'
+import ListItem from './ListItem'
+import movieData from '../data/movies.json'
+
+class List extends Component{
+    constructor(){
+        super()
+        this.state = {
+            movies: movieData
+         }
+    
+    }
+
+
+    render(){
+        const moviesMap = this.state.movies.map(movie => {
+            return <ListItem key={movie.id} movie={movie}/>
+        })
+        return(
+        <div>{moviesMap}</div>
+        )
+    }
+
+}
+
+
+
+export default List
